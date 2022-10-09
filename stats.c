70c5b67ca696fa81404992f690d33a359af908ce
Rakesh Singha <r.singha.official@gmail.com>
2022-10-09 16:17:33 +0530

    Adding the function definitions for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -50,30 +50,77 @@ void main() {
 
 /* Add other Implementation File Code Here */
 void print_statistics(unsigned char *arr, unsigned int len){
-
+  printf("Minimum - %d \n", find_minimum(arr, len));
+  printf("Maximum - %d \n", find_maximum(arr, len));
+  printf("Mean    - %d \n", find_mean(arr, len));
+  printf("Median  - %d \n", find_median(arr, len));  
+  return;
 }
 
 void print_array(unsigned char *arr, unsigned int len){
-
+  for(int i =0; i<len; i++)
+  {
+    printf("\nArray[%d]  -  %d", i, arr[i]);
+  }
+  printf("\n");
+  return;
 }
 
 int find_median(unsigned char *arr, unsigned int len){
+  unsigned char arr2[SIZE];
+  for(int i=0; i<len;i++)
+  {
+    arr2[i] = arr[i];
+  }
+  sort_array(arr2, len);
 
+  if(len%2==0)
+    return (arr2[(len/2)-1] + arr2[len/2])/2;
+  else
+    return arr2[(len-1)/2];  
 }
 
 int find_mean(unsigned char *arr, unsigned int len){
-
+  int sum = 0;
+  for(int i=0; i<len;i++)
+  {
+    sum = sum + arr[i];
+  }
+  return sum/len;  
 }
 
 int find_maximum(unsigned char *arr, unsigned int len){
-
+  int max=arr[0];
+  for(int i=1; i<len; i++)
+  {
+    if(arr[i]>max)
+      max = arr[i];
+  }
+  return max;
 }
 
 int find_minimum(unsigned char *arr, unsigned int len){
-
+  int min = arr[0];
+  for(int i=1; i<len; i++)
+  {
+    if(arr[i]<min)
+      min = arr[i];
+  }
+  return min;
 }
 
 void sort_array(unsigned char *arr, unsigned int len){
-
+  int i, key, j;
+  for (i = 1; i < len; i++) {
+    key = arr[i];
+    j = i - 1;
+  
+    while (j >= 0 && arr[j] < key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  return;
 }
 
